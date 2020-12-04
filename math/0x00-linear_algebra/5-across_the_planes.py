4c64b0fa84f55c37ef6e0c2c8b8c11c234257c41
Diego <vandeldiegoc@gmail.com>
2020-12-03 23:40:16 -0500

    fix[5] output

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -6,12 +6,12 @@ def add_matrices2D(mat1, mat2):
     """ add matrices """
     if len(mat1) != len(mat2):
         return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
     new_array = []
     for inx in range(len(mat1[0])):
-        if len(mat1[0]) != len(mat2[0]):
-            return None
         temp = []
         for j in range(len(mat1)):
             temp.append(mat1[inx][j] + mat2[inx][j])
         new_array.append(temp)
-    return new_array
+    return(new_array)
