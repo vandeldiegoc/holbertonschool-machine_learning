8d990d7e899fbc6cb69aca57fd72af1fb61ce314
Diego <vandeldiegoc@gmail.com>
2020-12-06 23:40:48 -0500

    add[12]

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" module """
+
+
+def np_elementwise(mat1, mat2):
+    """ funtion """
+
+    add = mat1+mat2
+    sub = mat1-mat2
+    mul = mat1*mat2
+    div = mat1/mat2
+    return(add, sub, mul, div)
