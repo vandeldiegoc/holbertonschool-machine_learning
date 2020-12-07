5afb20a283c7ccc3d9169fe80d1740ef2f71f901
Diego <vandeldiegoc@gmail.com>
2020-12-06 23:22:09 -0500

    variable name

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -11,4 +11,4 @@ def mat_mul(mat1, mat2):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 result[i][j] += mat1[i][k] * mat2[k][j]
-    return(result)
+    return result
