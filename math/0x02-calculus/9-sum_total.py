bc2a39a3150059775cfb4161e8067be7fa63d05f
Diego <vandeldiegoc@gmail.com>
2020-12-10 18:12:25 -0500

    9[add]

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+"""module """
+
+
+def summation_i_squared(n):
+    """ Sums of powers """
+    if type(n) != int or n <= 0:
+        return None
+    m = n * (n + 1)*(2 * n + 1) / 6
+    return(m)
