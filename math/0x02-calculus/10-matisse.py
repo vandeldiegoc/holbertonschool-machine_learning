21b792a3c091eb18da96431e3ea0db79143e5f2d
Diego <vandeldiegoc@gmail.com>
2020-12-10 18:53:21 -0500

    10[add]

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module """
+
+
+def poly_derivative(poly):
+    """that calculates the derivative of a polynomial """
+    new_list = []
+    if type(poly) != list or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return[0]
+    for x in range(1, len(poly)):
+        print(poly[x])
+        new_list.append(x * poly[x])
+    return(new_list)
