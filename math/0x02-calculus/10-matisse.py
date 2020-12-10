1cbaf2d2e844172627c3006076f83f539e4fb808
Diego <vandeldiegoc@gmail.com>
2020-12-10 18:57:18 -0500

    10[fix]

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -8,7 +8,7 @@ def poly_derivative(poly):
     if type(poly) != list or len(poly) == 0:
         return None
     if len(poly) == 1:
-        return[0]
+        return [0]
     for x in range(1, len(poly)):
         new_list.append(x * poly[x])
     return(new_list)
