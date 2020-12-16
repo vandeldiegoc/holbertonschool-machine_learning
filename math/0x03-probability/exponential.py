1a3cb59cc65f6dbe981f42a3a1aed4d245574700
Diego <vandeldiegoc@gmail.com>
2020-12-16 12:49:08 -0500

    exponential[update] pdf

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -4,6 +4,8 @@
 
 class Exponential:
     """represents an exponential distribution:"""
+    euler = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         if data is None:
             if lambtha <= 0:
@@ -15,3 +17,8 @@ class Exponential:
             if len(data) <= 1:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / float(sum(data) / len(data))
+
+    def pdf(self, x):
+        """Calculates the value of the PMF """
+        pdf = self.lambtha * (self.euler ** ((-self.lambtha) * x))
+        return(pdf)
