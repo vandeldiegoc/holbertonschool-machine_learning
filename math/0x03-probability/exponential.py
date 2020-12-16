d4f700d188f899e2bc547ac82aa1b3bb3a2b6439
Diego <vandeldiegoc@gmail.com>
2020-12-16 12:13:52 -0500

    exponentian[add]

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+class Exponential:
+    """represents an exponential distribution:"""
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) <= 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = 100 / float(sum(data))
