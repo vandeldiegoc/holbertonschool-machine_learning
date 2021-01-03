3080e8ca3c0d76fa427481b29300b53a776967fb
Diego <vandeldiegoc@gmail.com>
2021-01-03 01:04:10 -0500

    all[fix] attribute

--- supervised_learning/0x01-classification/3-neuron.py
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -10,9 +10,9 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx <= 0:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.normal(size=(1, nx))
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
