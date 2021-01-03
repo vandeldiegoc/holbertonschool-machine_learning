0c1959581e38f78304a9d77c4f35434c1e1503f5
Diego <vandeldiegoc@gmail.com>
2021-01-03 00:29:24 -0500

    all[change] permission

--- supervised_learning/0x01-classification/2-neuron.py
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
+"""module"""
 import numpy as np
 
 
 class Neuron:
-    """
-    class that work like a neurron
-    """
-    e = 2.7182818285
-
+    """class that work like a neurron"""
     def __init__(self, nx):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        if nx < 0:
+        if nx <= 0:
             raise ValueError("nx must be a positive integer")
-        self.__W = np.random.normal(size=(1, nx))
-        self.__b = 0
-        self.__A = 0
-
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
     @property
     def W(self):
         return(self.__W)
