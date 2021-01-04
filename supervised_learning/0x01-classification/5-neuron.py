a7f132af7a2af7344d4d0517ad8e1f4684778668
Diego <vandeldiegoc@gmail.com>
2021-01-03 20:46:26 -0500

    [delete] train funtion

--- supervised_learning/0x01-classification/5-neuron.py
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -57,18 +57,3 @@ class Neuron:
         db = np.sum(dz) / m
         self.__W = self.__W - (alpha * dw).T
         self.__b = self.__b - alpha * db
-
-    def train(self, X, Y, iterations=5000, alpha=0.05):
-        """ trains neuron"""
-        if not isinstance(iterations, int):
-            raise TypeError("iterations must be an integer")
-        if iterations < 1:
-            raise ValueError("iterations must be a positive integer")
-        if not isinstance(alpha, float):
-            raise TypeError("alpha must be a float")
-        if alpha <= 0:
-            raise ValueError("alpha must be positive")
-        for i in range(iterations):
-            self.forward_prop(X)
-            self.gradient_descent(X, Y, self.__A, alpha)
-        return self.evaluate(X, Y)
