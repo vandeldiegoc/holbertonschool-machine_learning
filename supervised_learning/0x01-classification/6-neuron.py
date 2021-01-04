0608ceaf4dcd985e1d190b2c54866960a4078981
Diego <vandeldiegoc@gmail.com>
2021-01-03 20:42:33 -0500

    add[6]

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -64,7 +64,36 @@ class Neuron:
             raise TypeError("iterations must be an integer")
         if iterations < 1:
             raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float)
+        """ Evaluates the neuron’s predictions """
+        a = self.forward_prop(X)
+        all_c = np.where(a <= 0.5, 0, 1)
+        eva = self.cost(Y, a)
+        return(all_c, eva)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ Calculates one pass of gradient descent on the neuron """
+        m = len(X[0])
+        dz = A - Y
+        dw = np.matmul(X, dz.T) / m
+        db = np.sum(dz) / m
+        self.__W = self.__W - (alpha * dw).T
+        self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """train neuron"""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for i in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A, alpha)
+        return self.evaluate(X, Y):
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
