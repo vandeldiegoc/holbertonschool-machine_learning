12d2ee98f366143893080fd89ae71aa8ef51bb35
Diego <vandeldiegoc@gmail.com>
2021-01-05 01:19:02 -0500

    18[add]

---
+++ supervised_learning/0x01-classification/18-deep_neural_network.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""module"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    class DeepNeuralNetwork that defines
+    a deep neural network performing binary
+    classification
+    """
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for p_i in range(len(layers)):
+            if (type(layers[p_i]) is not int or layers[p_i] < 1):
+                raise TypeError("layers must be a list of positive integers")
+            key_W = "W{}".format(p_i + 1)
+            key_b = "b{}".format(p_i + 1)
+            if p_i == 0:
+                w = np.random.randn(layers[p_i], nx) * np.sqrt(2 / nx)
+                self.weights[key_W] = w
+            else:
+                heteal_2 = np.sqrt(2 / layers[p_i - 1])
+                w = np.random.randn(layers[p_i], layers[p_i - 1]) * heteal_2
+                self.weights[key_W] = w
+            b = np.zeros((layers[p_i], 1))
+            self.weights[key_b] = b
+
+    @property
+    def L(self):
+        """getter L1"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """getter cache"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """getter weights"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+        self.__cache['A0'] = X
+        for l in range(self.__L):
+            z1 = np.matmul(self.__weights['W' + str(l + 1)],
+                           self.__cache['A' + str(l)])
+            Z = z1 + self.__weights['b' + str(l + 1)]
+            self.__cache['A' + str(l + 1)] = 1 / (1 + np.exp(-Z))
+
+        return(self.__cache['A' + str(l + 1)], self.__cache)
