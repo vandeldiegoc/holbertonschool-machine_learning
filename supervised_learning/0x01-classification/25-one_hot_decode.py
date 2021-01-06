c2504aee1bc2c7cc1f192b21ed8e14d33b12707e
Diego <vandeldiegoc@gmail.com>
2021-01-06 01:06:14 -0500

    add[25]

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -7,6 +7,8 @@ def one_hot_decode(one_hot):
     """ one_hot_decode """
     try:
         class_labels = np.argmax(one_hot, axis=0)
+        if type(class_labels) != np.ndarray:
+            return(None)
         return(class_labels)
     except Exception:
         return(None)
