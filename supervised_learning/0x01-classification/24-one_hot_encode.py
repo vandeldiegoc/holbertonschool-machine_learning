173091cdb359422a3a466c1d5d02202a3a1ad36c
Diego <vandeldiegoc@gmail.com>
2021-01-06 00:23:47 -0500

    24[add]

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Convert array of indices to one-hot encoded numpy array """
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """funtion one_hot_encode """
+    res = np.eye(classes)[np.array(Y).reshape(-1)]
+    return res.reshape(list(Y.shape)+[classes]).T
