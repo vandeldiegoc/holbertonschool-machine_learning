29e422ecabe5cfee1768d4918ec88a9b97774095
vandel <vandeldiegoc@gmail.com>
2021-01-23 22:49:02 -0500

    1[add]

---
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" module """
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    """ that normalizes (standardizes) a matrix:"""
+    nomal = (X - s) / m
+    return(nomal)
