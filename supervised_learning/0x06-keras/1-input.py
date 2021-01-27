3fb131d4f517bdf5ae2343a20ce722f5020c09cd
vandel <vandeldiegoc@gmail.com>
2021-01-26 21:51:48 -0500

    add[1]

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ that builds a neural network with the Keras library """
+    L2 = keras.regularizers.L1L2(l2=lambtha)
+
+    inputs = keras.Input(shape=(nx,))
+
+    layer = tf.keras.layers.Dense(units=layers[0],
+                                  activation=activations[0],
+                                  kernel_regularizer=L2)(inputs)
+    print(len(layers))
+
+    for i in range(1, len(layers)):
+
+        layer = keras.layers.Dropout(1 - keep_prob)(layer)
+        layer = keras.layers.Dense(units=layers[i],
+                                   activation=activations[i],
+                                   kernel_regularizer=L2, )(layer)
+    model = keras.Model(inputs=inputs, outputs=layer)
+    return model
