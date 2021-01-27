1e5fd3945f674b4e6474d5ebae7bc386ec9abae5
vandel <vandeldiegoc@gmail.com>
2021-01-26 22:21:42 -0500

    add[1]

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -10,16 +10,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     inputs = k.Input(shape=(nx,))
 
     layer = k.layers.Dense(units=layers[0],
-                               activation=activations[0],
-                               kernel_regularizer=L2,
-                               input_shape=(nx,))(inputs)
-    print(len(layers))
+                           activation=activations[0],
+                           kernel_regularizer=L2,
+                           input_shape=(nx,))(inputs)
 
     for i in range(1, len(layers)):
 
         layer = k.layers.Dropout(1 - keep_prob)(layer)
         layer = k.layers.Dense(units=layers[i],
-                                   activation=activations[i],
-                                   kernel_regularizer=L2)(layer)
+                               activation=activations[i],
+                               kernel_regularizer=L2)(layer)
     model = k.Model(inputs=inputs, outputs=layer)
     return model
