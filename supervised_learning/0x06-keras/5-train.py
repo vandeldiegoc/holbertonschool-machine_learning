423365360c260a4a4c7d830eb288ca5f771a729f
vandel <vandeldiegoc@gmail.com>
2021-01-29 17:26:59 -0500

    5[add]

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -5,7 +5,7 @@ import tensorflow.keras as k
 
 
 def train_model(network, data, labels, batch_size,
-                epochs, validation_data=None, 
+                epochs, validation_data=None,
                 verbose=True, shuffle=False):
     """  that trains a model using mini-batch gradient descent: """
     output = network.fit(data,
