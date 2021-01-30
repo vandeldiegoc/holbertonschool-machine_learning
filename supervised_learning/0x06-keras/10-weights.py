2ceb19e69cf6ca393031b32941ce0f7db6b9a400
vandel <vandeldiegoc@gmail.com>
2021-01-29 23:07:48 -0500

    10[add]

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module"""
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """saves a model’s weights"""
+    network.save_weights(filename, save_format=save_format)
+    return(None)
+
+
+def load_weights(network, filename):
+    """loads a model’s weights"""
+    network.load_weights(filename)
+    return(None)
