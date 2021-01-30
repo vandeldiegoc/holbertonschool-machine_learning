13d00680f3085d2f36816167a596008abb92c107
vandel <vandeldiegoc@gmail.com>
2021-01-29 19:08:34 -0500

    add[6]

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -8,7 +8,7 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """  that trains a model using mini-batch gradient descent: """
-    if validation_data and early_stopping == True:
+    if validation_data:
         es = [k.callbacks.EarlyStopping(monitor='val_loss',
                                         patience=patience)]
 
