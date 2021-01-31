dcc86d7ff79b015ced39bb5d431d1c20a4abeadf
vandel <vandeldiegoc@gmail.com>
2021-01-31 14:50:19 -0500

    5[add]

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -4,35 +4,41 @@ import numpy as np
 
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """that performs a convolution on grayscale images"""
-    m, h, w, c = images.shape
-    hh, wh, cc, kk = kernels.shape
-    sw, sh = stride
+    """ that performs a convolution on images using multiple kernels: """
+    w, h, m = images.shape[2], images.shape[1], images.shape[0]
+    kk, kw, kh = kernels.shape[3], kernels.shape[1], kernels.shape[0]
+    sw, sh = stride[1], stride[0]
 
-    ph = 0
-    pw = 0
+    pw, ph = 0, 0
 
     if padding == 'same':
-        ph = int(((h - 1) * sh + hh - h) / 2) + 1
-        pw = int(((w - 1) * sw + wh - w) / 2) + 1
+        ph = int(((h - 1) * sh + kh - h) / 2) + 1
+        pw = int(((w - 1) * sw + kw - w) / 2) + 1
 
-    if type(padding) == tuple:
+    if isinstance(padding, tuple):
         ph = padding[0]
         pw = padding[1]
 
-    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+    images = np.pad(images,
+                    pad_width=((0, 0),
+                               (ph, ph),
+                               (pw, pw),
+                               (0, 0)),
                     mode='constant', constant_values=0)
 
-    ouput_h = int(((h + 2 * ph - hh) / sh) + 1)
-    ouput_w= int(((w + 2 * pw - wh) / sw) + 1)
+    new_h = int(((h + 2 * ph - kh) / sh) + 1)
+    new_w = int(((w + 2 * pw - kw) / sw) + 1)
 
-    ouput = np.zeros((m, ouput_h, ouput_w, kk))
+    output = np.zeros((m, new_h, new_w, kk))
 
-    for y in range(ouput_h):
-        for x in range(ouput_w):
+    for y in range(new_h):
+        for x in range(new_w):
             for k in range(kk):
-                ouput[:, y, x, k] = (kernels[:, :, :, k] * images[:,
-                                     y*(stride[0]): y*(stride[0]) + hh,
-                                     x*(stride[1]): x*(stride[1]) + wh, :])\
-                                        .sum(axis=(1, 2, 3))
-    return(ouput)
+                output[:, y, x, k] = \
+                    (kernels[:, :, :, k] *
+                     images[:,
+                     y * sh: y * sh + kh,
+                     x * sw: x * sw + kw,
+                     :]).sum(axis=(1, 2, 3))
+
+    return output
