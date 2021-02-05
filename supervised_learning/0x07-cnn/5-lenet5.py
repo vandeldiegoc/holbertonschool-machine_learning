0029e1f3b3c1abe1a59f25ad9220af9cfb36362f
vandel <vandeldiegoc@gmail.com>
2021-02-04 23:56:41 -0500

    5[add]

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -7,13 +7,12 @@ import tensorflow.keras as K
 def lenet5(x):
     """hat builds a modified version
        of the LeNet-5 architecture using keras """
-    initializer = K.initializers.he_normal(seed=None)
 
     layer = K.layers.Conv2D(filters=6,
                             kernel_size=(5, 5),
                             padding='same',
                             activation='relu',
-                            kernel_initializer=initializer,
+                            kernel_initializer='he_normal',
                             )(x)
 
     layer = K.layers.MaxPool2D(pool_size=(2, 2),
@@ -23,7 +22,7 @@ def lenet5(x):
                             kernel_size=(5, 5),
                             padding='valid',
                             activation='relu',
-                            kernel_initializer=initializer,
+                            kernel_initializer='he_normal',
                             )(layer)
 
     layer = K.layers.MaxPool2D(pool_size=(2, 2),
@@ -34,17 +33,17 @@ def lenet5(x):
 
     layer = K.layers.Dense(units=120,
                            activation='relu',
-                           kernel_initializer=initializer,
+                           kernel_initializer='he_normal',
                            )(layer)
 
     layer = K.layers.Dense(units=84,
                            activation='relu',
-                           kernel_initializer=initializer,
+                           kernel_initializer='he_normal',
                            )(layer)
 
     layer = K.layers.Dense(units=10,
                            activation='softmax',
-                           kernel_initializer=initializer,
+                           kernel_initializer='he_normal',
                            )(layer)
 
     network = K.Model(inputs=x, outputs=layer)
