30da59a75c761bf9c7958082205614e9d7eaeeaf
vandel <vandeldiegoc@gmail.com>
2021-02-04 23:29:11 -0500

    5[add]

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """module """
+
 import tensorflow.keras as K
 
 
@@ -9,42 +10,42 @@ def lenet5(x):
     initializer = K.initializers.he_normal(seed=None)
 
     layer = K.layers.Conv2D(filters=6,
-                               kernel_size=(5, 5),
-                               padding='same',
-                               activation='relu',
-                               kernel_initializer=initializer,
-                               )(x)
+                            kernel_size=(5, 5),
+                            padding='same',
+                            activation='relu',
+                            kernel_initializer=initializer,
+                            )(x)
 
     layer = K.layers.MaxPool2D(pool_size=(2, 2),
-                                  strides=(2, 2))(layer)
+                               strides=(2, 2))(layer)
 
     layer = K.layers.Conv2D(filters=16,
-                               kernel_size=(5, 5),
-                               padding='valid',
-                               activation='relu',
-                               kernel_initializer=initializer,
-                               )(layer)
+                            kernel_size=(5, 5),
+                            padding='valid',
+                            activation='relu',
+                            kernel_initializer=initializer,
+                            )(layer)
 
     layer = K.layers.MaxPool2D(pool_size=(2, 2),
-                                  strides=(2, 2),
-                                  )(layer)
+                               strides=(2, 2),
+                               )(layer)
 
     layer = K.layers.Flatten()(layer)
 
     layer = K.layers.Dense(units=120,
-                              activation='relu',
-                              kernel_initializer=initializer,
-                              )(layer)
+                           activation='relu',
+                           kernel_initializer=initializer,
+                           )(layer)
 
     layer = K.layers.Dense(units=84,
-                              activation='relu',
-                              kernel_initializer=initializer,
-                              )(layer)
+                           activation='relu',
+                           kernel_initializer=initializer,
+                           )(layer)
 
     layer = K.layers.Dense(units=10,
-                              activation='softmax',
-                              kernel_initializer=initializer,
-                              )(layer)
+                           activation='softmax',
+                           kernel_initializer=initializer,
+                           )(layer)
 
     network = K.Model(inputs=x, outputs=layer)
 
