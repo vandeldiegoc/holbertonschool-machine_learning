e581b29edf1f7aa7338d507d806a99188807a2e9
vandel <vandeldiegoc@gmail.com>
2021-02-04 23:24:53 -0500

    5[add]

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -3,50 +3,50 @@
 import tensorflow.keras as K
 
 
-def lenet5(x, y):
+def lenet5(x):
     """hat builds a modified version
        of the LeNet-5 architecture using keras """
     initializer = K.initializers.he_normal(seed=None)
 
-    my_layer = K.layers.Conv2D(filters=6,
+    layer = K.layers.Conv2D(filters=6,
                                kernel_size=(5, 5),
                                padding='same',
                                activation='relu',
                                kernel_initializer=initializer,
                                )(x)
 
-    my_layer = K.layers.MaxPool2D(pool_size=(2, 2),
-                                  strides=(2, 2))(my_layer)
+    layer = K.layers.MaxPool2D(pool_size=(2, 2),
+                                  strides=(2, 2))(layer)
 
-    my_layer = K.layers.Conv2D(filters=16,
+    layer = K.layers.Conv2D(filters=16,
                                kernel_size=(5, 5),
                                padding='valid',
                                activation='relu',
                                kernel_initializer=initializer,
-                               )(my_layer)
+                               )(layer)
 
-    my_layer = K.layers.MaxPool2D(pool_size=(2, 2),
+    layer = K.layers.MaxPool2D(pool_size=(2, 2),
                                   strides=(2, 2),
-                                  )(my_layer)
+                                  )(layer)
 
-    my_layer = K.layers.Flatten()(my_layer)
+    layer = K.layers.Flatten()(layer)
 
-    my_layer = K.layers.Dense(units=120,
+    layer = K.layers.Dense(units=120,
                               activation='relu',
                               kernel_initializer=initializer,
-                              )(my_layer)
+                              )(layer)
 
-    my_layer = K.layers.Dense(units=84,
+    layer = K.layers.Dense(units=84,
                               activation='relu',
                               kernel_initializer=initializer,
-                              )(my_layer)
+                              )(layer)
 
-    my_layer = K.layers.Dense(units=10,
+    layer = K.layers.Dense(units=10,
                               activation='softmax',
                               kernel_initializer=initializer,
-                              )(my_layer)
+                              )(layer)
 
-    network = K.Model(inputs=x, outputs=my_layer)
+    network = K.Model(inputs=x, outputs=layer)
 
     network.compile(optimizer=K.optimizers.Adam(),
                     loss='categorical_crossentropy',
