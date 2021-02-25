caef0ae993b568dd01abe1d1f8d1062e8a550b1e
vandel <vandeldiegoc@gmail.com>
2021-02-25 09:14:47 -0500

    0[add]

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -3,13 +3,12 @@
 import tensorflow.keras as K
 
 
-class  Yolo:
+class Yolo:
     """class Yolo that uses the Yolo v3
        algorithm to perform object detection"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
-            print(f)
             self.class_names = f.read().splitlines()
         self.class_t = class_t
         self.nms_t = nms_t
