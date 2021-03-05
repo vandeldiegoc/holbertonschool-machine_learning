cc318daaac17c624b41d01131ff4e8f8b19c6da1
vandel <vandeldiegoc@gmail.com>
2021-03-05 12:22:38 -0500

    0[add]

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -4,8 +4,7 @@ import numpy as np
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
-    """ l2_reg_cost - calculates the cost of a neural network
-    """
+    """ l2_reg_cost - calculates the cost of a neural network"""
     layers = 0
     for i in range(1, L + 1):
         k = 'W{}'.format(i)
