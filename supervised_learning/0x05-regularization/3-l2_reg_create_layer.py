2fd63e197dfc3469d871a45b2df7c03817b6ae1a
vandel <vandeldiegoc@gmail.com>
2021-03-05 12:50:30 -0500

    3[add]

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """ creates a tensorflow layer 
+    """ creates a tensorflow layer
         that includes L2 regularization """
     reg = tf.contrib.layers.l2_regularizer(lambtha)
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
