cff9d7e8ffca4695aff87530e48f64d05f066fef
vandel <vandeldiegoc@gmail.com>
2021-03-05 12:53:28 -0500

    4[add]

---
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""xmodule"""
+
+import numpy as np
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """return dictionary containing the outputs
+    of each layer and the dropout"""
+    cache = {'A0': X}
+
+    for i in range(L):
+        W_key = "W{}".format(i + 1)
+        b_key = "b{}".format(i + 1)
+        A_key_prev = "A{}".format(i)
+        A_key_forw = "A{}".format(i + 1)
+        D_key = "D{}".format(i + 1)
+
+        Z = np.matmul(weights[W_key], cache[A_key_prev]) \
+            + weights[b_key]
+        drop = np.random.binomial(1, keep_prob, size=Z.shape)
+
+        if i == L - 1:
+            t = np.exp(Z)
+            cache[A_key_forw] = (t / np.sum(t, axis=0, keepdims=True))
+        else:
+            cache[A_key_forw] = np.tanh(Z)
+            cache[D_key] = drop
+            cache[A_key_forw] = (cache[A_key_forw] * cache[D_key]) / keep_prob
+
+    return cache
