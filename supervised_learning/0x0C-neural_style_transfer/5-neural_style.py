12e588e3fe9908a913608f45ae32efa18194d7bd
vandel <vandeldiegoc@gmail.com>
2021-03-04 23:06:02 -0500

    5[add]

--- supervised_learning/0x0C-neural_style_transfer/5-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/5-neural_style.py
@@ -145,5 +145,4 @@ class NST:
             style_costs.append(weighted_layer_style_cost)
 
         style_cost = tf.add_n(style_costs)
-        print(style_cost)
-        return style_cost + 38200.0
+        return style_cost
