13939855f8059da75892bfaf47e836c6a04984ad
vandel <vandeldiegoc@gmail.com>
2021-04-08 23:08:03 -0500

    2[delete] printf

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -13,17 +13,10 @@ def regular(P):
     if len(P.shape) != 2 or P.shape[0] != P.shape[1]:
         return None
     if ((P > 0).all()):
-        print('p', P)
         dim = P.shape[0]
-        print(dim)
         q = (P-np.eye(dim))
-        print('q', q)
         ones = np.ones(dim)
-        print('one', ones)
         q = np.c_[q, ones]
-        print('q', q)
         QTQ = np.dot(q, q.T)
-        print('QTQ', QTQ)
         bQT = np.ones(dim)
-        print('bQt', bQT)
         return np.linalg.solve(QTQ, bQT)
