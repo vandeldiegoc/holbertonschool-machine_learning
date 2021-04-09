6251b72a9a37f13786bf7b5cded2e443040b52ae
vandel <vandeldiegoc@gmail.com>
2021-04-08 22:57:55 -0500

    0, 1, 2[add]

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -8,19 +8,17 @@ def markov_chain(P, s, t=1):
     """ that determines the probability of a markov
         chain being in a particular state after a
         specified number of iteration """
-    if t < 0 or not isinstance(t, int):
+   x if (not isinstance(P, np.ndarray) or not isinstance(s, np.ndarray)):
         return None
-    if not isinstance(P, np.ndarray):
+
+    if (not isinstance(t, int)):
         return None
-    if len(P.shape) != 2 or P.shape[0] != P.shape[1]:
+
+    if ((P.ndim != 2) or (s.ndim != 2) or (t < 1)):
         return None
-    if len(s.shape) != 2 or s.shape[0] != 1 or s.shape[1] != P.shape[0]:
-        return None
-    if not isinstance(s, np.ndarray):
-        return None
-    if (P < 0).any():
-        return None
-    if not isinstance(t, int):
+
+    n = P.shape[0]
+    if (P.shape != (n, n)) or (s.shape != (1, n)):
         return None
     for i in range(t):
         s = np.matmul(s, P)
