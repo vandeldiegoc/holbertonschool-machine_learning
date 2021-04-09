b5a701eb0523c73d2fb1c56e9fe8db7242ebeb96
vandel <vandeldiegoc@gmail.com>
2021-04-08 23:14:34 -0500

    2[delete] printf

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -15,11 +15,23 @@ def regular(P):
     n = P.shape[0]
     if not np.isclose(np.sum(P, axis=1), np.ones(n))[0]:
         return None
-    if ((P > 0).all()):
-        dim = P.shape[0]
-        q = (P-np.eye(dim))
-        ones = np.ones(dim)
-        q = np.c_[q, ones]
-        QTQ = np.dot(q, q.T)
-        bQT = np.ones(dim)
-        return np.linalg.solve(QTQ, bQT)
+    #if ((P > 0).all()):
+    #    dim = P.shape[0]
+    #    q = (P-np.eye(dim))
+    #    ones = np.ones(dim)
+    #    q = np.c_[q, ones]
+    #    QTQ = np.dot(q, q.T)
+    #    bQT = np.ones(dim)
+    #    return np.linalg.solve(QTQ, bQT)
+    s = np.full(n, (1 / n))[np.newaxis, ...]
+    Pk = np.copy(P)
+    s_prev = s
+
+    while True:
+        Pk = np.matmul(Pk, P)
+        if np.any(Pk <= 0):
+            return None
+        s = np.matmul(s, P)
+        if np.all(s_prev == s):
+            return s
+        s_prev = s
