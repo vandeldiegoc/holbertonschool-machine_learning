62ed16274aaa9fab03541a13e2bf14d7ac3dd67a
vandel <vandeldiegoc@gmail.com>
2021-04-22 20:06:07 -0500

    0[add]

---
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -0,0 +1,26 @@
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """ that creates an autoencoder """
+    Dense = keras.layers.Dense
+    Model = keras.Model
+
+    input_img = keras.Input((input_dims,))
+    encoded = input_img
+    for hidden_layer in hidden_layers:
+        encoded = Dense(hidden_layer, activation='relu')(encoded)
+    encoded = Dense(latent_dims, activation='relu')(encoded)
+
+    encoded_img = keras.Input((latent_dims,))
+    decoded = encoded_img
+    for hidden_layer in reversed(hidden_layers):
+        decoded = Dense(hidden_layer, activation='relu')(decoded)
+    decoded = Dense(input_dims, activation="sigmoid")(decoded)
+
+    encoder = Model(input_img, encoded)
+    decoder = Model(encoded_img, decoded)
+    out_decoder = decoder(encoder(input_img))
+    auto = Model(input_img, out_decoder)
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+    return encoder, decoder, auto
