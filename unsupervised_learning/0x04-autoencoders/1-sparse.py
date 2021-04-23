373a3540ea095d972eb5c189d6330289c325f496
vandel <vandeldiegoc@gmail.com>
2021-04-22 20:21:46 -0500

    1[add]

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -3,7 +3,7 @@
 import tensorflow.keras as keras
 
 
-def autoencoder(input_dims, hidden_layers, latent_dims):
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     """ that creates an autoencoder """
     Dense = keras.layers.Dense
     Model = keras.Model
@@ -12,10 +12,10 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     input_img = keras.Input((input_dims,))
     encoded = input_img
     for hidden_layer in hidden_layers:
-        encoded = Dense(hidden_layer,
-                        activation='relu',
-                        activity_regularizer=regularizers(encoded))(encoded)
-    encoded = Dense(latent_dims, activation='relu')(encoded)
+        encoded = Dense(hidden_layer, activation='relu')(encoded)
+    encoded = Dense(latent_dims,
+                    activation='relu',
+                    activity_regularizer=regularizers)(encoded)
 
     encoded_img = keras.Input((latent_dims,))
     decoded = encoded_img
