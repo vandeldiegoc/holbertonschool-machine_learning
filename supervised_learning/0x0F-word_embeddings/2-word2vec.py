3a3d0040ee7454aa96a6a4f3a372781cabbd9ed0
vandel <vandeldiegoc@gmail.com>
2021-05-11 23:07:24 -0500

    4[add]

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -7,10 +7,7 @@ def word2vec_model(sentences, size=100, min_count=5, window=5,
                    negative=5, cbow=True, iterations=5, seed=0,
                    workers=1):
     """ Creates and trains gensim word2vec model"""
-    if cbow is False:
-        sg = 1
-    else:
-        sg = 0
+    sg = not cbow
     model = Word2Vec(sentences=sentences, size=size,
                      min_count=min_count, window=window,
                      negative=negative, sg=sg,
--- supervised_learning/0x0F-word_embeddings/4-fasttext.py
+++ supervised_learning/0x0F-word_embeddings/4-fasttext.py
@@ -7,10 +7,7 @@ def fasttext_model(sentences, size=100, min_count=5, window=5,
                    negative=5, cbow=True, iterations=5, seed=0,
                    workers=1):
     """ Creates and trains gensim FastText model"""
-    if cbow is False:
-        sg = 1
-    else:
-        sg = 0
+    sg = not cbow
     model = FastText(sentences=sentences, size=size,
                      min_count=min_count, window=window,
                      negative=negative, sg=sg,
