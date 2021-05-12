e0ab4f1706aa3683e617a182b48f4d0c4f4fb879
vandel <vandeldiegoc@gmail.com>
2021-05-11 22:58:29 -0500

    1[fix]

--- supervised_learning/0x0F-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -5,11 +5,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 def tf_idf(sentences, vocab=None):
     """ that creates a bag of words embedding matrix:"""
-    vectorizer = TfidfVectorizer()
-    if vocab is None:
-        X = vectorizer.fit_transform(sentences)
-    else:
-        X = vectorizer.fit_transform(vocab)
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
     e = X.toarray()
     f = vectorizer.get_feature_names()
     return e, f
