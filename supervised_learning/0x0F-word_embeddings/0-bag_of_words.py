18465de88dcbfe44689af162c9a814201ac3cf54
vandel <vandeldiegoc@gmail.com>
2021-05-18 22:10:46 -0500

    0-bag[fix] typing

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -2,6 +2,7 @@
 """module"""
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 def bag_of_words(sentences, vocab=None):
     """ that creates a bag of words embedding matrix:"""
     vectorizer = CountVectorizer(vocabulary=vocab)
