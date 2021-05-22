2f6230ef0fd85736aa15d52ce2b14c75e3a5f961
vandel <vandeldiegoc@gmail.com>
2021-05-21 23:37:22 -0500

    1[fix] pep8 and comment

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -2,6 +2,7 @@
 """ class """
 import tensorflow as tf
 
+
 class SelfAttention(tf.keras.layers.Layer):
     """ to calculate the attention"""
     def __init__(self, units):
@@ -10,19 +11,12 @@ class SelfAttention(tf.keras.layers.Layer):
         self.W = tf.keras.layers.Dense(units=units)
         self.U = tf.keras.layers.Dense(units=units)
         self.V = tf.keras.layers.Dense(units=1, activation='relu')
+
     def call(self, s_prev, hidden_states):
         """ calculate self-atention """
-        h = self.W(hidden_states)
-        s_newdims = tf.expand_dims(s_prev, axis=1)
-        s = self.U(s_newdims)
-        e = self.V(h+s)
-        a = tf.keras.activations.softmax(e)
+        s_newdims = self.W(tf.expand_dims(s_prev, axis=1))
+        h = self.U(hidden_states)
+        e = self.V(tf.tanh(h+s_newdims))
+        a = tf.keras.activations.softmax(e, axis=1)
         c = tf.reduce_sum(a * hidden_states, 1)
-        return c, e
-
-
-
-
-
-        
-
+        return c, a
