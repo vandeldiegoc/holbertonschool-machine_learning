90a18d2c7faef83b4e5609de8f88d25930acb098
vandel <vandeldiegoc@gmail.com>
2021-05-21 23:17:49 -0500

    1[fix] pep8 and comment

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -1,25 +1,25 @@
-import numpy as np
+#!/usr/bin/env python3
+""" class """
 import tensorflow as tf
 
 class SelfAttention(tf.keras.layers.Layer):
+    """ to calculate the attention"""
     def __init__(self, units):
+        """ constructor """
         super(SelfAttention, self).__init__()
         self.W = tf.keras.layers.Dense(units=units)
         self.U = tf.keras.layers.Dense(units=units)
         self.V = tf.keras.layers.Dense(units=1, activation='relu')
     def call(self, s_prev, hidden_states):
-       h = self.W(hidden_states)
-       s_newdims = tf.expand_dims(s_prev, axis=1)
-       s = self.U(s_newdims)
+        """ calculate self-atention """
+        h = self.W(hidden_states)
+        s_newdims = tf.expand_dims(s_prev, axis=1)
+        s = self.U(s_newdims)
+        e = self.V(h+s)
+        a = tf.keras.activations.softmax(e)
+        c = tf.reduce_sum(a * hidden_states, 1)
+        return c, e
 
-       e = self.V(h+s)
-       a = tf.keras.activations.softmax(e)
-       print(a.shape)
-       print(hidden_states.shape)
-       c = tf.reduce_sum(a * hidden_states, 1)
-       return c, e
-
-    
 
 
 
