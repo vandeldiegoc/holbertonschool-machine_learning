16a1a53c4112d3fc5589d570c145fbce64466206
vandel <vandeldiegoc@gmail.com>
2021-05-23 22:52:54 -0500

    add batch public instance

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -7,24 +7,25 @@ SelfAttention = __import__('1-self_attention').SelfAttention
 class RNNDecoder(tf.keras.layers.Layer):
     """ class """
     def __init__(self, vocab, embedding, units, batch):
-        """ Class constructor"""
+        """contrutor """
         super(RNNDecoder, self).__init__()
-
+        self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-
-        self.gru = tf.keras.layers.GRU(units,
-                                       recurrent_initializer='glorot_uniform',
-                                       return_sequences=True,
-                                       return_state=True)
+        self.gru = tf.keras.layers.GRU(
+             units=units,
+             recurrent_initializer='glorot_uniform',
+             return_sequences=True,
+             return_state=True
+             )
         self.F = tf.keras.layers.Dense(vocab)
 
     def call(self, x, s_prev, hidden_states):
-        """method """
-        attention = SelfAttention(s_prev.shape[1])
-        context, weights = attention(s_prev, hidden_states)
+        """decode"""
+        encode = SelfAttention(self.units)
+        c, prev_state = encode(s_prev, hidden_states)
         x = self.embedding(x)
-        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
-        outputs, s = self.gru(x)
-        outputs = tf.reshape(outputs, (-1, outputs.shape[2]))
-        y = self.F(outputs)
+        inputs = tf.concat([tf.expand_dims(c, 1), x], axis=-1)
+        decode_output, s = self.gru(inputs)
+        output = tf.reshape(decode_output, (-1, decode_output.shape[2]))
+        y = self.F(output)
         return y, s
