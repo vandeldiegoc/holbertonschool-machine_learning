63820a2e7b376e3b6de1fcb19dbdd7f56d5abac2
vandel <vandeldiegoc@gmail.com>
2021-05-23 23:57:03 -0500

    4[add]

---
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""modile"""
+
+
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """ calculates the positional
+        encoding for a transformer: """
+    angle_rads = get_angles(np.arange(max_seq_len)[:, np.newaxis],
+                            np.arange(dm)[np.newaxis, :],
+                            dm)
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return pos_encoding[0]
+
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
