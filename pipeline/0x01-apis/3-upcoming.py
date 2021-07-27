8bf3c11c8e0a562a0dc27c67094f3570786eff40
vandel <vandeldiegoc@gmail.com>
2021-07-26 20:15:08 -0500

    3[fix] pep8

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""Program that By using the (unofficial) SpaceX API, write a script
-that displays the upcoming launch with these information"""
-import requests
+"""script that displays the upcoming launch with these information"""
 import sys
+import requests as rq
 import time
 
 if __name__ == '__main__':
     url = "https://api.spacexdata.com/v4/launches/upcoming"
-    r = requests.get(url)
+    r = rq.get(url)
     launches = sorted(r.json(), key=lambda i: i['date_unix'])
     date_unix = launches[0]['date_unix']
     for i in r.json():
@@ -18,10 +17,10 @@ if __name__ == '__main__':
             launchpad_id = i['launchpad']
             break
     url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
-    r = requests.get(url)
+    r = rq.get(url)
     rocket_name = r.json()['name']
     url = "https://api.spacexdata.com/v4/launchpads/{}".format(launchpad_id)
-    r = requests.get(url)
+    r = rq.get(url)
     lpad_name = r.json()['name']
     lpad_locality = r.json()['locality']
     print("{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
