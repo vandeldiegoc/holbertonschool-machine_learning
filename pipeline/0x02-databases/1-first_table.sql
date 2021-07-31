f042b796d423e6d3cd4dd17529b939c24a4c7235
vandel <vandeldiegoc@gmail.com>
2021-07-31 17:40:53 -0500

    1 [fix] comment

--- pipeline/0x02-databases/1-first_table.sql
+++ pipeline/0x02-databases/1-first_table.sql
@@ -1,4 +1,4 @@
--- creates a table called first_table in the current database in your MySQL server.
+-- creates a table called first_table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     mame VARCHAR(256) 
