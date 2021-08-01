987df434c5b02a2754c4bbc5ab15008829616915
vandel <vandeldiegoc@gmail.com>
2021-07-31 22:59:52 -0500

    10[add]

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 --  and displays the number of shows linked to each
-SELECT tv_genres.name as genre, count(*) as number_of_shows 
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows 
 FROM tv_genres, tv_show_genres, tv_shows
 WHERE tv_genres.id=tv_show_genres.genre_id 
 AND tv_shows.id=tv_show_genres.show_id 
