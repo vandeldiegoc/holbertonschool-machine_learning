338b22a83872b4b7d751df9b9c7693b1c9b82563
vandel <vandeldiegoc@gmail.com>
2021-08-02 01:38:23 -0500

    17- 21[add]

---
+++ pipeline/0x02-databases/19-bonus.sql
@@ -0,0 +1,11 @@
+-- stored procedure AddBonus that adds a new correction for a student.
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name varchar(255), IN score INT)
+BEGIN
+DECLARE projectId INT;
+IF NOT EXISTS(SELECT id FROM projects WHERE name = project_name) THEN
+INSERT INTO  projects (name) VALUES (project_name);
+END IF;
+SET projectId = (SELECT id FROM projects WHERE name = project_name);
+INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, projectId, score);
+END;
+DELIMITER ;
