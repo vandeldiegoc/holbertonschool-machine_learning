0b4c387eedb656b7b4b110cb38235e4a15cf6b93
vandel <vandeldiegoc@gmail.com>
2021-08-02 22:59:41 -0500

    106[add]

---
+++ pipeline/0x02-databases/106-log_stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""by adding the top 10 of the most present IPs in 
+   the collection nginx of the database logs """
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":  
+    client = MongoClient()
+    collection_logs = client.logs.nginx
+    num_logs = collection_logs.count_documents({})
+
+    print("{} logs".format(num_logs))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+
+    for elem in methods:
+        docs_method = collection_logs.count_documents({'method': elem})
+        print("\tmethod {}: {}".format(elem, docs_method))
+
+    my_query= {"method": "GET", "path": "/status"}
+    status_check = collection_logs.count_documents(my_query)
+
+    print("{} status check".format(status_check))
+
+    print('IPs:')
+
+    pipeline = [
+        {"$sortByCount": '$ip'},
+        {"$limit": 10},
+        {"$sort": {"ip": -1}},
+    ]
+    ips = collection_logs.aggregate(pipeline)
+
+    for ip in ips:
+        print("\t{}: {}".format(ip['_id'], ip['count']))
